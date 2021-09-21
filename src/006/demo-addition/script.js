2a7242823fa1eac830c9bb5a19562a315bdb47dd
Dan Gilleland <dgilleland@nait.ca>
2021-09-21 09:24:39 -0600

    First demo of Lesson 006
    ... with a bug to be fixed

--- src/006/demo-addition/script.js
+++ src/006/demo-addition/script.js
@@ -1,1 +1,16 @@
 // Add in the code from the walk-through and observe the page behaviour.
+let first = prompt('Enter a number.');
+let second = prompt('Enter another number.');
+
+let result = first + second;
+
+alert(`I calculated a result of ${result} when adding the numbers.`);
+
+firstNumber.innerText = first;
+secondNumber.innerText = second;
+answer.innerText = result;
+
+let repeat = confirm('Again?');
+
+let para = document.querySelector('p'); // grab the first <p>
+para.innerHTML = repeat ? 'Press <a href="#">this link</a> to try again.' : 'Thanks for playing';

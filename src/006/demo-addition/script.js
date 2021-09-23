ced9b2a30ace501e8999b798fb787396745f3e2a
Dan Gilleland <dgilleland@nait.ca>
2021-09-23 09:16:31 -0600

    Turn addition into a general math calculator

--- src/006/demo-addition/script.js
+++ src/006/demo-addition/script.js
@@ -1,20 +1,52 @@
+// Create our own Math functions for two numbers
+const addNumbers = function (firstParam, secondParam) {
+    let result = 0;
+    result = result + parseFloat(firstParam);
+    result += parseFloat(secondParam);
+    return result;
+}
+const multiplyNumbers = function (firstParam, secondParam) {
+    return parseFloat(firstParam) * parseFloat(secondParam);
+}
+const subtractNumbers = function (firstParam, secondParam) {
+    return parseFloat(firstParam) - parseFloat(secondParam);
+}
+const divideNumbers = function (firstParam, secondParam) {
+    return parseFloat(firstParam) / parseFloat(secondParam);
+}
+
+const setUp = function() {
+    // Ask for two numbers, and put them inside the <output> elements
+    let first = prompt('Enter a number.');
+    let second = prompt('Enter another number.');
+    firstNumber.innerText = first;
+    secondNumber.innerText = second;
+}
+
+const showAnswer = function(result) {
+    answer.innerText = result;
+}
+
+const doMath = function(title, symbol, mathFunction) {
+    operation.innerText = title;
+    operator.innerText = symbol;
+    let result = mathFunction(firstNumber.innerText, secondNumber.innerText);
+    showAnswer(result);
+}
 // Add in the code from the walk-through and observe the page behaviour.
-let first = prompt('Enter a number.');
-let second = prompt('Enter another number.');
 
 // ensure that I have a numeric data type
-first = parseFloat(first);
-second = parseFloat(second);
+// first = parseFloat(first);
+// second = parseFloat(second);
 
-let result = first + second;
+// let result = first + second;
 
-alert(`I calculated a result of ${result} when adding the numbers.`);
+// alert(`I calculated a result of ${result} when adding the numbers.`);
 
-firstNumber.innerText = first;
-secondNumber.innerText = second;
-answer.innerText = result;
 
-let repeat = confirm('Again?');
+// answer.innerText = result;
 
-let para = document.querySelector('p'); // grab the first <p>
-para.innerHTML = repeat ? 'Press <a href="#">this link</a> to try again.' : 'Thanks for playing';
+// let repeat = confirm('Again?');
+
+// let para = document.querySelector('p'); // grab the first <p>
+// para.innerHTML = repeat ? 'Press <a href="#">this link</a> to try again.' : 'Thanks for playing';

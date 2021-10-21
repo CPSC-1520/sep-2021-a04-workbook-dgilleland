c1d83f25c3e03d70a6c88cfec7f3723743ebddd9
Dan Gilleland <dgilleland@nait.ca>
2021-10-21 10:15:40 -0600

    Completed lesson 040

--- src/040/demos/name-list/js/main.js
+++ src/040/demos/name-list/js/main.js
@@ -1,20 +1,27 @@
 /*
 	TASK: prompt for individual names and add them to 
-		  a list.  Confirm after each name is entered
-		  whether the user would like to enter another
-		  name or not.
+		a list.  Confirm after each name is entered
+		whether the user would like to enter another
+		name or not.
 */
-let outDiv = document.querySelector('div');
+const myProgram = function(evt) {
+	let outDiv = document.querySelector('div');
 
-let myHTML = '<ul>';
+	let myHTML = '';
 
-// Add my names
-do {
-    let aName = prompt('Enter name: ');
-    myHTML += '<li>' + aName + '</li>';
-} while(confirm('Another name?'));
+	// Add my names
+	do {
+		let aName = prompt('Enter name: ');
+		myHTML += `<li>${aName}</li>`; // Template String
+	} while(confirm('Another name?'));
 
-myHTML += '</ul>';
+	myHTML = `<ul>${myHTML}</ul>`; // Template String
 
-// display the list
-outDiv.innerHTML = myHTML;
+	// display the list
+	outDiv.innerHTML = myHTML;
+}
+
+// By explicitly waiting for the DOMContentLoaded event,
+// I can "defer" my program code from running until after
+// the DOM (the <body>) is loaded.
+document.addEventListener('DOMContentLoaded', myProgram);
